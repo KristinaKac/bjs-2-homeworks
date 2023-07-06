52390d235c6f32f7c98f3f4c86c6bb901a34cf6d
Kristina Kachalkova <kachalkovakristina@mail.ru>
2023-07-06 18:34:03 +0300

    async task is ready

---
+++ 7.async/task.js
@@ -0,0 +1,59 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if (this.alarmCollection.includes(time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+        let clock = {
+            callback,
+            time,
+            canCall: true,
+        }
+        this.alarmCollection.push(clock);
+    }
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(item => !(item.time === time));
+
+        this.alarmCollection.filter((item, index, arr) => {
+            if (item.time === time) {
+                arr.splice(arr[index], 1);
+            }
+        });
+    }
+    getCurrentFormattedTime() {
+        let date = new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+        return date;
+    }
+    start() {
+        if (this.intervalId != null) {
+            return;
+        }
+        this.intervalId = setInterval(() =>
+            this.alarmCollection.forEach(item => {
+                if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+                    item.canCall = false;
+                    item.callback();
+                }
+            }), 1000);
+    }
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+    resetAllCalls() {
+        this.alarmCollection.forEach(item => item.canCall = true);
+    }
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}

7fc36f3f9b6abc13d58ec76a7ef98dfc4237d433
Kristina Kachalkova <kachalkovakristina@mail.ru>
2023-07-12 20:42:58 +0300

    async is fixed

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,59 +1,60 @@
 class AlarmClock {
     constructor() {
-        this.alarmCollection = [];
-        this.intervalId = null;
+      this.alarmCollection = [];
+      this.intervalId = null;
     }
     addClock(time, callback) {
-        if (!time || !callback) {
-            throw new Error('Отсутствуют обязательные аргументы');
-        }
-        if (this.alarmCollection.includes(time)) {
-            console.warn('Уже присутствует звонок на это же время');
-        }
-        let clock = {
-            callback,
-            time,
-            canCall: true,
-        }
-        this.alarmCollection.push(clock);
+      if (!time || !callback) {
+        throw new Error('Отсутствуют обязательные аргументы');
+      }
+      const haveCall = this.alarmCollection.some((item) => time.hasOwnProperty(item.time));
+  
+      if (haveCall) {
+        console.warn('Уже присутствует звонок на это же время');
+      }
+      let clock = {
+        callback,
+        time,
+        canCall: true,
+      }
+      this.alarmCollection.push(clock);
     }
     removeClock(time) {
-        this.alarmCollection = this.alarmCollection.filter(item => !(item.time === time));
-
-        this.alarmCollection.filter((item, index, arr) => {
-            if (item.time === time) {
-                arr.splice(arr[index], 1);
-            }
-        });
+      this.alarmCollection = this.alarmCollection.filter((item, index, arr) => {
+        if (item.time === time) {
+          arr.splice(arr[index], 1);
+        }
+        return !(item.time === time);
+      });
     }
     getCurrentFormattedTime() {
-        let date = new Date().toLocaleTimeString("ru-Ru", {
-            hour: "2-digit",
-            minute: "2-digit",
-        });
-        return date;
+      let date = new Date().toLocaleTimeString("ru-Ru", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      return date;
     }
     start() {
-        if (this.intervalId != null) {
-            return;
-        }
-        this.intervalId = setInterval(() =>
-            this.alarmCollection.forEach(item => {
-                if (item.time === this.getCurrentFormattedTime() && item.canCall) {
-                    item.canCall = false;
-                    item.callback();
-                }
-            }), 1000);
+      if (this.intervalId != null) {
+        return;
+      }
+      this.intervalId = setInterval(() =>
+        this.alarmCollection.forEach(item => {
+          if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+            item.canCall = false;
+            item.callback();
+          }
+        }), 1000);
     }
     stop() {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
+      clearInterval(this.intervalId);
+      this.intervalId = null;
     }
     resetAllCalls() {
-        this.alarmCollection.forEach(item => item.canCall = true);
+      this.alarmCollection.forEach(item => item.canCall = true);
     }
     clearAlarms() {
-        this.stop();
-        this.alarmCollection = [];
+      this.stop();
+      this.alarmCollection = [];
     }
-}
+  }
